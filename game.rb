63166110e88359cf53566669a83427f64c203cf3
LagoLunatic <LagoLunatic@users.noreply.github.com>
2016-02-22 19:45:58 -0500

    Allow initializing Game from folder or rom

--- game.rb
+++ game.rb
@@ -4,17 +4,24 @@ class Game
   
   attr_reader :areas, :fs
   
-  def initialize(input_rom_path)
-    case File.read(input_rom_path, 12)
-    when "CASTLEVANIA1"
-      require_relative './constants/dos_constants.rb'
-    when "CASTLEVANIA2"
-      require_relative './constants/por_constants.rb'
-    when "CASTLEVANIA3"
-      require_relative './constants/ooe_constants.rb'
-    else
-      raise InvalidGameError.new("Specified game is not a DSVania.")
+  def initialize_from_folder(input_folder_path)
+    header_path = File.join(input_folder_path, "ftc", "ndsheader.bin")
+    unless File.exist?(header_path) && File.file?(header_path)
+      raise "Header file not present"
     end
+    verify_game_and_load_constants(header_path)
+    
+    @fs = NDSFileSystem.new
+    fs.open_directory(input_folder_path)
+    CONSTANT_OVERLAYS.each do |overlay_index|
+      fs.load_overlay(overlay_index)
+    end
+    
+    read_from_rom()
+  end
+  
+  def initialize_from_rom(input_rom_path)
+    verify_game_and_load_constants(input_rom_path)
     
     folder = File.dirname(input_rom_path)
     folder = File.join(folder, "extracted_files_#{GAME}")
@@ -52,5 +59,20 @@ class Game
     
     # Make all areas on the world map accessible.
     fs.write(0x020AA8E4, [0xE3A00001].pack("V"))
+  end
+  
+private
+  
+  def verify_game_and_load_constants(header_path)
+    case File.read(header_path, 12)
+    when "CASTLEVANIA1"
+      require_relative './constants/dos_constants.rb'
+    when "CASTLEVANIA2"
+      require_relative './constants/por_constants.rb'
+    when "CASTLEVANIA3"
+      require_relative './constants/ooe_constants.rb'
+    else
+      raise InvalidGameError.new("Specified game is not a DSVania.")
+    end
   end
 end
