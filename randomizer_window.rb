00931953ec45d37879948178ff570ef62bcd1a0b
LagoLunatic <LagoLunatic@users.noreply.github.com>
2016-02-29 20:27:56 -0500

    Randomizer UI: Remember settings across sessions

--- randomizer_window.rb
+++ randomizer_window.rb
@@ -16,25 +16,22 @@ class RandomizerWindow < Qt::Dialog
     
     load_settings()
     
-    if @settings[:clean_rom_path]
-      @ui.clean_rom.setText(@settings[:clean_rom_path])
-    end
-    if @settings[:output_folder]
-      @ui.output_folder.setText(@settings[:output_folder])
-    end
-    if @settings[:seed]
-      @ui.seed.setText(@settings[:seed])
-    end
-    
     connect(@ui.clean_rom, SIGNAL("editingFinished()"), self, SLOT("update_settings()"))
     connect(@ui.output_folder, SIGNAL("editingFinished()"), self, SLOT("update_settings()"))
     connect(@ui.seed, SIGNAL("editingFinished()"), self, SLOT("update_settings()"))
+    
+    connect(@ui.randomize_items, SIGNAL("stateChanged(int)"), self, SLOT("update_settings()"))
+    connect(@ui.randomize_souls_relics_and_glyphs, SIGNAL("stateChanged(int)"), self, SLOT("update_settings()"))
+    connect(@ui.randomize_enemies, SIGNAL("stateChanged(int)"), self, SLOT("update_settings()"))
+    connect(@ui.randomize_bosses, SIGNAL("stateChanged(int)"), self, SLOT("update_settings()"))
+    connect(@ui.randomize_enemy_drops, SIGNAL("stateChanged(int)"), self, SLOT("update_settings()"))
+    connect(@ui.randomize_boss_souls, SIGNAL("stateChanged(int)"), self, SLOT("update_settings()"))
+    connect(@ui.randomize_doors, SIGNAL("stateChanged(int)"), self, SLOT("update_settings()"))
+    connect(@ui.randomize_starting_room, SIGNAL("stateChanged(int)"), self, SLOT("update_settings()"))
+    
     connect(@ui.submit, SIGNAL("clicked()"), self, SLOT("randomize()"))
     
     self.show()
-    
-    #settings = YAML::load_file("settings.yml")
-    #open_rom(settings[:input_rom_paths]["dos"])
   end
   
   def load_settings
@@ -44,6 +41,19 @@ class RandomizerWindow < Qt::Dialog
     else
       @settings = {}
     end
+    
+    @ui.clean_rom.setText(@settings[:clean_rom_path]) if @settings[:clean_rom_path]
+    @ui.output_folder.setText(@settings[:output_folder]) if @settings[:output_folder]
+    @ui.seed.setText(@settings[:seed]) if @settings[:seed]
+    
+    @ui.randomize_items.setChecked(@settings[:randomize_items]) unless @settings[:randomize_items].nil?
+    @ui.randomize_souls_relics_and_glyphs.setChecked(@settings[:randomize_souls_relics_and_glyphs]) unless @settings[:randomize_souls_relics_and_glyphs].nil?
+    @ui.randomize_enemies.setChecked(@settings[:randomize_enemies]) unless @settings[:randomize_enemies].nil?
+    @ui.randomize_bosses.setChecked(@settings[:randomize_bosses]) unless @settings[:randomize_bosses].nil?
+    @ui.randomize_enemy_drops.setChecked(@settings[:randomize_enemy_drops]) unless @settings[:randomize_enemy_drops].nil?
+    @ui.randomize_boss_souls.setChecked(@settings[:randomize_boss_souls]) unless @settings[:randomize_boss_souls].nil?
+    @ui.randomize_doors.setChecked(@settings[:randomize_doors]) unless @settings[:randomize_doors].nil?
+    @ui.randomize_starting_room.setChecked(@settings[:randomize_starting_room]) unless @settings[:randomize_starting_room].nil?
   end
   
   def closeEvent(event)
@@ -56,6 +66,15 @@ class RandomizerWindow < Qt::Dialog
     @settings[:clean_rom_path] = @ui.clean_rom.text
     @settings[:output_folder] = @ui.output_folder.text
     @settings[:seed] = @ui.seed.text
+    
+    @settings[:randomize_items] = @ui.randomize_items.checked
+    @settings[:randomize_souls_relics_and_glyphs] = @ui.randomize_souls_relics_and_glyphs.checked
+    @settings[:randomize_enemies] = @ui.randomize_enemies.checked
+    @settings[:randomize_bosses] = @ui.randomize_bosses.checked
+    @settings[:randomize_enemy_drops] = @ui.randomize_enemy_drops.checked
+    @settings[:randomize_boss_souls] = @ui.randomize_boss_souls.checked
+    @settings[:randomize_doors] = @ui.randomize_doors.checked
+    @settings[:randomize_starting_room] = @ui.randomize_starting_room.checked
   end
   
   def randomize
@@ -98,20 +117,6 @@ class RandomizerWindow < Qt::Dialog
     game.fs.write_to_rom(output_rom_path)
 
     Qt::MessageBox.information(self, "Done", "Randomization complete.")
-  end
-  
-  def open_rom_dialog
-    rom_path = Qt::FileDialog.getOpenFileName(self, "Select ROM", nil, "NDS ROM Files (*.nds)")
-    return if rom_path.nil?
-    folder = Qt::FileDialog.getExistingDirectory(self, "Select folder to extract files to")
-    return if folder.nil?
-    open_rom(rom_path, folder)
-  end
-  
-  def open_folder_dialog
-    folder = Qt::FileDialog.getExistingDirectory(self, "Open folder")
-    return if folder.nil?
-    open_folder(folder)
   end
 end
 
