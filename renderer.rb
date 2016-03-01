e47474ee6b52c52f38f7c633df355943dc36434d
LagoLunatic <LagoLunatic@users.noreply.github.com>
2016-03-01 15:43:27 -0500

    Reorder renderer functions

--- renderer.rb
+++ renderer.rb
@@ -69,53 +69,12 @@ class Renderer
     return rendered_layer
   end
   
-  def generate_palettes(palette_data_start_offset, colors_per_palette)
-    if palette_data_start_offset.nil?
-      palette = [ChunkyPNG::Color.rgba(0, 0, 0, 0)] * colors_per_palette
-      palette_list = [palette] * 128 # 128 is the maximum number of palettes
-      return palette_list
+  def get_tileset(pointer_to_tileset_for_layer, palette_offset, graphic_tilesets_for_room, colors_per_palette, collision_tileset_offset, tileset_filename)
+    if File.exist?(tileset_filename)
+      ChunkyPNG::Image.from_file(tileset_filename)
+    else
+      render_tileset(pointer_to_tileset_for_layer, palette_offset, graphic_tilesets_for_room, colors_per_palette, collision_tileset_offset, tileset_filename)
     end
-    
-    number_of_palettes = fs.read(palette_data_start_offset+2,1).unpack("C*").first
-    if number_of_palettes == 1
-      # When this value is 1, it means this tileset has no palette of its own. Instead it simply leeches off the palette of whatever the previously loaded room was.
-      # Since we can't know what room the player was in last, we can instead simply use the palette of whatever room this room's first door leads to.
-      # This should work fine in theory. But there's a tiny possibility of one of the first door leading to an unused, inaccessible room, which may not have the right palette.
-      
-      # TODO: the above can only be done once functions are organized in such a way that we can get the doors for this room and the palette of another room.
-      # the below is just a hack. it works for the only room in dawn of sorrow that needs this anyway.
-      number_of_palettes = 0x40
-      palette_data_start_offset -= 0x804
-    end
-    
-    #puts "graphic_tile_data_start_offset: %08X" % graphic_tile_data_start_offset
-    palette_data_start_offset += 4 # Skip the first 4 bytes, as they contain the length of this palette page, not the palette data itself.
-
-    #puts "palette_data_start_offset: %08X" % palette_data_start_offset
-    #puts "number_of_palettes: %d" % number_of_palettes
-    palette_list = []
-    (0..number_of_palettes).each do |palette_index| # todo: cache palettes
-      palette_data = fs.read(palette_data_start_offset + 32*palette_index, colors_per_palette*2)
-      palette = palette_data.scan(/.{2}/m).map do |color|
-        color = color.unpack("v*").first
-        # these two bytes hold the rgb data for the color in this format:
-        # ?bbbbbgggggrrrrr
-        # the ? is unknown.
-        #unknown_bit = (color >> 15) & 0b0000_0000_0000_0001
-        blue_bits   = (color >> 10) & 0b0000_0000_0001_1111
-        green_bits  = (color >> 5)  & 0b0000_0000_0001_1111
-        red_bits    =  color        & 0b0000_0000_0001_1111
-        
-        red = (red_bits / 32.0 * 255).to_i
-        green = (green_bits / 32.0 * 255).to_i
-        blue = (blue_bits / 32.0 * 255).to_i
-        alpha = 255
-        ChunkyPNG::Color.rgba(red, green, blue, alpha)
-      end
-      palette_list << palette
-    end
-    
-    palette_list
   end
   
   def ensure_tilesets_exist(folder, room)
@@ -126,14 +85,6 @@ class Renderer
       else
         render_tileset(layer.ram_pointer_to_tileset_for_layer, room.palette_offset, room.graphic_tilesets_for_room, layer.colors_per_palette, tileset_filename)
       end
-    end
-  end
-  
-  def get_tileset(pointer_to_tileset_for_layer, palette_offset, graphic_tilesets_for_room, colors_per_palette, collision_tileset_offset, tileset_filename)
-    if File.exist?(tileset_filename)
-      ChunkyPNG::Image.from_file(tileset_filename)
-    else
-      render_tileset(pointer_to_tileset_for_layer, palette_offset, graphic_tilesets_for_room, colors_per_palette, collision_tileset_offset, tileset_filename)
     end
   end
   
@@ -184,6 +135,120 @@ class Renderer
     rendered_tileset.save(output_filename, :fast_rgba)
     puts "Wrote #{output_filename}"
     return rendered_tileset
+  end
+  
+  def render_graphic_tile(graphic_tile_data_file, palette, tile_index_on_page)
+    graphic_tile = ChunkyPNG::Image.new(16, 16, ChunkyPNG::Color::TRANSPARENT)
+    
+    if graphic_tile_data_file.nil? || palette.nil?
+      # This room has no graphics, so just return a black tile. Could be a save room, warp room, transition room, etc.
+      # TODO: instead it would be better to use the debug squares for these rooms so you can see what's what.
+      
+      graphic_tile = ChunkyPNG::Image.new(16, 16, ChunkyPNG::Color.rgba(255, 0, 0, 255))
+      return graphic_tile
+    end
+    
+    x_block_on_tileset = tile_index_on_page % 8
+    y_block_on_tileset = tile_index_on_page / 8
+    if palette.length == 16
+      size_of_block_in_bytes = 16 * 16 / 2 # 16 pixels tall, 16 pixels wide, each byte has 2 pixels in it
+    elsif palette.length == 256
+      size_of_block_in_bytes = 16 * 16 # 16 pixels tall, 16 pixels wide, each byte has 1 pixel in it
+    else
+      raise "Unknown palette length: #{palette.length}"
+    end
+    size_of_row_in_bytes = 8 * size_of_block_in_bytes
+    
+    if palette.length == 16
+      bytes_per_16_pixels = 8
+    elsif palette.length == 256
+      bytes_per_16_pixels = 16
+    else
+      raise "Unknown palette length: #{palette.length}"
+    end
+    
+    offset = y_block_on_tileset*size_of_row_in_bytes + x_block_on_tileset*bytes_per_16_pixels
+    pixel_on_tile = 0
+    (0..15).each do |i|
+      pixels_for_chunky = []
+      
+      fs.rom[graphic_tile_data_file[:start_offset] + offset, bytes_per_16_pixels].each_byte do |byte|
+        if palette.length == 16
+          pixels = [byte & 0b00001111, byte >> 4] # get the low 4 bits, then the high 4 bits (it's reversed). each is one pixel, two pixels total inside this byte.
+        elsif palette.length == 256
+          pixels = [byte]
+        else
+          raise "Unknown palette length: #{palette.length}"
+        end
+        
+        pixels.each do |pixel|
+          x_pixel_on_tile = pixel_on_tile % 16
+          y_pixel_on_tile = pixel_on_tile / 16
+          pixel_on_tile += 1
+          
+          if pixel == 0 # transparent
+            pixels_for_chunky << ChunkyPNG::Color::TRANSPARENT
+          else
+            pixel_color = palette[pixel]
+            pixels_for_chunky << pixel_color
+          end
+        end
+      end
+      
+      graphic_tile.replace_row!(i, pixels_for_chunky)
+      offset += 8 * bytes_per_16_pixels
+    end
+    
+    return graphic_tile
+  end
+  
+  def generate_palettes(palette_data_start_offset, colors_per_palette)
+    if palette_data_start_offset.nil?
+      palette = [ChunkyPNG::Color.rgba(0, 0, 0, 0)] * colors_per_palette
+      palette_list = [palette] * 128 # 128 is the maximum number of palettes
+      return palette_list
+    end
+    
+    number_of_palettes = fs.read(palette_data_start_offset+2,1).unpack("C*").first
+    if number_of_palettes == 1
+      # When this value is 1, it means this tileset has no palette of its own. Instead it simply leeches off the palette of whatever the previously loaded room was.
+      # Since we can't know what room the player was in last, we can instead simply use the palette of whatever room this room's first door leads to.
+      # This should work fine in theory. But there's a tiny possibility of one of the first door leading to an unused, inaccessible room, which may not have the right palette.
+      
+      # TODO: the above can only be done once functions are organized in such a way that we can get the doors for this room and the palette of another room.
+      # the below is just a hack. it works for the only room in dawn of sorrow that needs this anyway.
+      number_of_palettes = 0x40
+      palette_data_start_offset -= 0x804
+    end
+    
+    #puts "graphic_tile_data_start_offset: %08X" % graphic_tile_data_start_offset
+    palette_data_start_offset += 4 # Skip the first 4 bytes, as they contain the length of this palette page, not the palette data itself.
+
+    #puts "palette_data_start_offset: %08X" % palette_data_start_offset
+    #puts "number_of_palettes: %d" % number_of_palettes
+    palette_list = []
+    (0..number_of_palettes).each do |palette_index| # todo: cache palettes
+      palette_data = fs.read(palette_data_start_offset + 32*palette_index, colors_per_palette*2)
+      palette = palette_data.scan(/.{2}/m).map do |color|
+        color = color.unpack("v*").first
+        # these two bytes hold the rgb data for the color in this format:
+        # ?bbbbbgggggrrrrr
+        # the ? is unknown.
+        #unknown_bit = (color >> 15) & 0b0000_0000_0000_0001
+        blue_bits   = (color >> 10) & 0b0000_0000_0001_1111
+        green_bits  = (color >> 5)  & 0b0000_0000_0001_1111
+        red_bits    =  color        & 0b0000_0000_0001_1111
+        
+        red = (red_bits / 32.0 * 255).to_i
+        green = (green_bits / 32.0 * 255).to_i
+        blue = (blue_bits / 32.0 * 255).to_i
+        alpha = 255
+        ChunkyPNG::Color.rgba(red, green, blue, alpha)
+      end
+      palette_list << palette
+    end
+    
+    palette_list
   end
   
   def render_collision_tileset(collision_tileset_offset, output_filename)
@@ -261,71 +326,6 @@ class Renderer
     rendered_tileset.save(output_filename, :fast_rgba)
     puts "Wrote #{output_filename}"
     return rendered_tileset
-  end
-  
-  def render_graphic_tile(graphic_tile_data_file, palette, tile_index_on_page)
-    graphic_tile = ChunkyPNG::Image.new(16, 16, ChunkyPNG::Color::TRANSPARENT)
-    
-    if graphic_tile_data_file.nil? || palette.nil?
-      # This room has no graphics, so just return a black tile. Could be a save room, warp room, transition room, etc.
-      # TODO: instead it would be better to use the debug squares for these rooms so you can see what's what.
-      
-      graphic_tile = ChunkyPNG::Image.new(16, 16, ChunkyPNG::Color.rgba(255, 0, 0, 255))
-      return graphic_tile
-    end
-    
-    x_block_on_tileset = tile_index_on_page % 8
-    y_block_on_tileset = tile_index_on_page / 8
-    if palette.length == 16
-      size_of_block_in_bytes = 16 * 16 / 2 # 16 pixels tall, 16 pixels wide, each byte has 2 pixels in it
-    elsif palette.length == 256
-      size_of_block_in_bytes = 16 * 16 # 16 pixels tall, 16 pixels wide, each byte has 1 pixel in it
-    else
-      raise "Unknown palette length: #{palette.length}"
-    end
-    size_of_row_in_bytes = 8 * size_of_block_in_bytes
-    
-    if palette.length == 16
-      bytes_per_16_pixels = 8
-    elsif palette.length == 256
-      bytes_per_16_pixels = 16
-    else
-      raise "Unknown palette length: #{palette.length}"
-    end
-    
-    offset = y_block_on_tileset*size_of_row_in_bytes + x_block_on_tileset*bytes_per_16_pixels
-    pixel_on_tile = 0
-    (0..15).each do |i|
-      pixels_for_chunky = []
-      
-      fs.rom[graphic_tile_data_file[:start_offset] + offset, bytes_per_16_pixels].each_byte do |byte|
-        if palette.length == 16
-          pixels = [byte & 0b00001111, byte >> 4] # get the low 4 bits, then the high 4 bits (it's reversed). each is one pixel, two pixels total inside this byte.
-        elsif palette.length == 256
-          pixels = [byte]
-        else
-          raise "Unknown palette length: #{palette.length}"
-        end
-        
-        pixels.each do |pixel|
-          x_pixel_on_tile = pixel_on_tile % 16
-          y_pixel_on_tile = pixel_on_tile / 16
-          pixel_on_tile += 1
-          
-          if pixel == 0 # transparent
-            pixels_for_chunky << ChunkyPNG::Color::TRANSPARENT
-          else
-            pixel_color = palette[pixel]
-            pixels_for_chunky << pixel_color
-          end
-        end
-      end
-      
-      graphic_tile.replace_row!(i, pixels_for_chunky)
-      offset += 8 * bytes_per_16_pixels
-    end
-    
-    return graphic_tile
   end
   
   def render_map(map, scale = 1)
