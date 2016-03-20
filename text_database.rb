ec98a239d821515b32a651783a69afccfb28d2b0
LagoLunatic <LagoLunatic@users.noreply.github.com>
2016-03-20 02:37:48 -0400

    Text Database: Fix DoS misc strings

--- text_database.rb
+++ text_database.rb
@@ -46,6 +46,12 @@ class TextDatabase
         next_string_ram_pointer = fs.expand_file_and_get_end_of_file_ram_address(text.string_ram_pointer, text.encoded_string.length + 3)
       end
       
+      # Misc strings in DoS must be aligned to the nearest 4 bytes or they won't be displayed.
+      region_name = TEXT_REGIONS.find{|name, range| range.include?(text.text_id)}[0]
+      if GAME == "dos" && region_name == "Misc" && next_string_ram_pointer % 4 != 0
+        next_string_ram_pointer = ((next_string_ram_pointer / 4) * 4) + 4
+      end
+      
       text.string_ram_pointer = next_string_ram_pointer
       text.write_to_rom()
       
