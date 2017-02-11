5d33381fcedcbbc68ce543641dcd61d4e55e9c3c
LagoLunatic <LagoLunatic@users.noreply.github.com>
2017-02-11 03:19:10 -0500

    Entity search: Allow searching by byte 5/8

--- dsvedit/entity_search_dialog.rb
+++ dsvedit/entity_search_dialog.rb
@@ -20,20 +20,24 @@ class EntitySearchDialog < Qt::Dialog
     @rooms = []
     @ui.room_list.clear()
     
+    byte_5 = @ui.byte_5.text =~ /^\h+$/ ? @ui.byte_5.text.to_i(16) : nil
     type = @ui.type.text =~ /^\h+$/ ? @ui.type.text.to_i(16) : nil
     subtype = @ui.subtype.text =~ /^\h+$/ ? @ui.subtype.text.to_i(16) : nil
+    byte_8 = @ui.byte_8.text =~ /^\h+$/ ? @ui.byte_8.text.to_i(16) : nil
     var_a = @ui.var_a.text =~ /^\h+$/ ? @ui.var_a.text.to_i(16) : nil
     var_b = @ui.var_b.text =~ /^\h+$/ ? @ui.var_b.text.to_i(16) : nil
     
-    if !type && !subtype && !var_a && !var_b
+    if !byte_5 && !type && !subtype && !byte_8 && !var_a && !var_b
       return
     end
     
     rooms = []
     parent.game.each_room do |room|
       room.entities.each do |entity|
+        next if byte_5 && byte_5 != entity.byte_5
         next if type && type != entity.type
         next if subtype && subtype != entity.subtype
+        next if byte_8 && byte_8 != entity.byte_8
         next if var_a && var_a != entity.var_a
         next if var_b && var_b != entity.var_b
         
