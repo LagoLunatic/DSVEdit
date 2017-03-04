d7bfefdf28f2f314a7b4784f25d37dbbe1c908ae
LagoLunatic <LagoLunatic@users.noreply.github.com>
2017-03-04 12:35:38 -0500

    Fix bug in dos seal drawing skip patch

--- asm/dos_skip_drawing_seals.asm
+++ asm/dos_skip_drawing_seals.asm
@@ -4,9 +4,13 @@
 
 .open "ftc/overlay9_0", 0219E3E0h
 
-; This makes it so you don't need to draw a Magic Seal to kill a boss.
+; This patch makes it so you don't need to draw a Magic Seal to kill a boss.
 
-.org 0x02213C04 ; Location of the boss-killed code for loading the current game mode.
+.org 0x02213C04 ; Location of the seal drawing code for loading the current game mode.
   mov r0, 1 ; Always load 1 (meaning Julius mode).
+
+; The above change causes a bug with the practice seal menu, where attempting to practice a seal will cause the screen to go black and the game to softlock.
+.org 0x021F1BF8 ; Location of code in the practice menu to decide which type of seal to do.
+  mov r1, 0 ; Instead of setting argument r1 to 2 (meaning a practice seal) we set it to 0 (meaning an automatically drawn example seal). This doesn't softlock the game.
 
 .close
