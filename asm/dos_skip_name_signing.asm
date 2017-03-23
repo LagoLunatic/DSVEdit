8af555e5373d7616ba5a43904d3b27db7c8d08ce
LagoLunatic <LagoLunatic@users.noreply.github.com>
2017-03-23 09:29:15 -0400

    Tweak to skip name signing patch

--- asm/dos_skip_name_signing.asm
+++ asm/dos_skip_name_signing.asm
@@ -9,4 +9,7 @@
 .org 0x02045E8C ; Code that sets the name signing menu's state to 3 after zeroing out the name pixel data.
   movne r0, 6h ; Instead set the state to 6, meaning the state where the player has pressed OK and the new game is now starting.
 
+.org 0x02045E78 ; Code that displays the name signing menu on the screen.
+  nop ; Delete it to prevent the name signing screen from being visible for a split second when starting a new game.
+
 .close
