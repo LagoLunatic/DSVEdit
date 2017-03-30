2503bb333740b55b83faa07b60a6f9f1c474854f
LagoLunatic <LagoLunatic@users.noreply.github.com>
2017-03-30 10:11:51 -0400

    Update DoS use what you see asm patch

--- asm/dos_use_what_you_see_souls.asm
+++ asm/dos_use_what_you_see_souls.asm
@@ -22,8 +22,42 @@
 .org 0x0202E304
   b 0202E1A0h ; Jump back to the code we skipped which causes the soul name to display in the upper right corner of the screen.
 
-.org 0x0221024C
-  mov r1,0h ; Store the number of this type of soul you have as 0. This causes the player's soul inventory to always be empty, so they can't equip anything manually.
-  ; TODO this is bad for ability souls
+.close
 
+.open "ftc/overlay9_0", 0219E3E0h
+
+.org 0x021F31EC ; Code that checks if the current equip menu has no entries in it. If it does, it doesn't let the player change equipment.
+  ldr r1, [r0,80h]
+  cmp r1, 0Bh ; Instead we check if the current equip menu is the item menu.
+  bne 021F3288h ; If it's not the item menu we don't let the player access this menu. This lets the player still change their equipped items, but not their equipped souls.
+
+; TODO when you enter a save/warp room for the first time it also shows the tutorial for all three types of souls. disable setting those flags to begin with.
+
+; TODO getting doppelganger gives you malphas for some reason, even if you have balore. this may be related to the tutorial shown in the save/warp room.
+
+.close
+
+; Make it so that bosses that drop red/blue/yellow souls required for progression don't stay dead. This is so the player can go back and fight them again if they lose a soul they need.
+; We do this by deleting the lines that set the boss death flags for each boss.
+.open "ftc/overlay9_30", 022FF9C0h ; Flying Armor
+.org 0x02300BBC
+  nop
+.close
+.open "ftc/overlay9_25", 022FF9C0h ; Puppet Master
+.org 0x022FFDC8
+  nop
+.org 0x022FFF4C
+  nop
+.close
+.open "ftc/overlay9_33", 022FF9C0h ; Zephyr
+.org 0x0230155C
+  nop
+.close
+.open "ftc/overlay9_37", 022FF9C0h ; Bat Company
+.org 0x022FFF3C
+  nop
+.close
+.open "ftc/overlay9_35", 022FF9C0h ; Paranoia
+.org 0x0230572C
+  nop
 .close
