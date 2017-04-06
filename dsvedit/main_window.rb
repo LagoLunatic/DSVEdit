e1e11c15b62e33bac4cadbd442ad7075ef8d8ae9
LagoLunatic <LagoLunatic@users.noreply.github.com>
2017-04-06 15:08:18 -0400

    Improve panning room

--- dsvedit/main_window.rb
+++ dsvedit/main_window.rb
@@ -70,7 +70,7 @@ class DSVEdit < Qt::MainWindow
     
     @room_graphics_scene = ClickableGraphicsScene.new
     @ui.room_graphics_view.setScene(@room_graphics_scene)
-    @ui.room_graphics_view.setDragMode(Qt::GraphicsView::ScrollHandDrag)
+    @ui.room_graphics_view.setFocus()
     self.setStyleSheet("QGraphicsView { background-color: transparent; }");
     connect(@room_graphics_scene, SIGNAL("clicked(int, int, const Qt::MouseButton&)"), self, SLOT("room_clicked(int, int, const Qt::MouseButton&)"))
     
@@ -1216,31 +1216,75 @@ class EntityRectItem < Qt::GraphicsRectItem
   end
 end
 
-class MiddleClickDragGraphicsView < Qt::GraphicsView
-  def mousePressEvent(event)
-    if event.button == Qt::MiddleButton
-      new_event = Qt::MouseEvent.new(event.type, event.pos, Qt::LeftButton, Qt::LeftButton, event.modifiers)
-      super(new_event)
+class RoomView < Qt::GraphicsView
+  def initialize(parent)
+    super(parent)
+    
+    # We want to receive mouseMoveEvents even when the user is not clicking any mouse buttons.
+    setMouseTracking(true)
+  end
+  
+  def keyPressEvent(event)
+    if event.key == Qt::Key_Space && !event.isAutoRepeat
+      set_panning(true)
+    end
+  end
+  
+  def keyReleaseEvent(event)
+    if event.key == Qt::Key_Space && !event.isAutoRepeat
+      set_panning(false)
+    end
+  end
+  
+  def set_panning(is_panning)
+    return if is_panning == @is_panning
+    
+    @is_panning = is_panning
+    
+    setInteractive(!is_panning)
+    
+    if is_panning
+      @orig_mouse_pose = Qt::Cursor.pos
+      Qt::Application.setOverrideCursor(Qt::Cursor.new(Qt::ClosedHandCursor))
+      #viewport.grabMouse()
     else
-      super(event)
+      #viewport.releaseMouse()
+      Qt::Application.restoreOverrideCursor()
     end
   end
   
   def mouseMoveEvent(event)
-    if event.button == Qt::MiddleButton
-      new_event = Qt::MouseEvent.new(event.type, event.pos, Qt::LeftButton, Qt::LeftButton, event.modifiers)
-      super(new_event)
+    if @is_panning
+      diff = event.globalPos - @orig_mouse_pose
+
+      horizontalValue = horizontalScrollBar.value - diff.x
+      verticalValue = verticalScrollBar.value - diff.y
+
+      horizontalScrollBar.setValue(horizontalValue)
+      verticalScrollBar.setValue(verticalValue)
+
+      @orig_mouse_pose = event.globalPos
     else
       super(event)
+      @orig_mouse_pose = event.globalPos
     end
+  end
+  
+  def mousePressEvent(event)
+    if event.button == Qt::MiddleButton
+      set_panning(true)
+      return
+    end
+    
+    super(event)
   end
   
   def mouseReleaseEvent(event)
     if event.button == Qt::MiddleButton
-      new_event = Qt::MouseEvent.new(event.type, event.pos, Qt::LeftButton, Qt::LeftButton, event.modifiers)
-      super(new_event)
-    else
-      super(event)
+      set_panning(false)
+      return
     end
+    
+    super(event)
   end
 end
