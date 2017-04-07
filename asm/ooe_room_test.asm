e94dc204523011cb1aa8bd3f23c1a95c4d104337
LagoLunatic <LagoLunatic@users.noreply.github.com>
2017-04-07 12:37:30 -0400

    Improve top screen for OoE room test

--- asm/ooe_room_test.asm
+++ asm/ooe_room_test.asm
@@ -4,10 +4,7 @@
 
 .open "ftc/overlay9_20", 021FFFC0h
 
-;.org 0x02213EC8 ; BUG: CRASH
-;.org 0x02213060 ; BUG: player can't control anything
-.org 0x022132F8 ; BUG: some items aren't equipped. top screen is black
-  ;b 0x02213EC8
+.org 0x022132F8 ; Code run for the intro logos.
   push r1, r14
   mov r0, 1h
   ldr r1, =021007D5h
@@ -16,12 +13,6 @@
   mov r0, 0h ; Save file 0.
   ldr r1, =02100144h
   bl 0202C610h
-  ;bl 020AD160h
-  ldr r1, =0210078Dh
-  ldrb r0, [r1]
-  mov r1, 10h
-  mov r2, 1h
-  bl 020657F8h
   ; Set the game state to 08 (ingame).
   mov r0, 8h
   mov r1, 10h
@@ -29,8 +20,8 @@
   bl 0202D7A0h
   ldr r1, =021006C0h
   ; Set the area, sector, and room indexes.
-  mov r0, 01h
-  strb r0, [r1, 0Ch] ; Area index, 021006CC
+  mov r2, 01h
+  strb r2, [r1, 0Ch] ; Area index, 021006CC
   mov r0, 02h
   strb r0, [r1, 0Dh] ; Sector index, 021006CD
   mov r0, 07h
@@ -41,6 +32,23 @@
   str r0, [r1, 00h] ; X pos, 021006C0
   ldr r0, =60001h
   str r0, [r1, 04h] ; Y pos, 021006C4
+  
+  cmp r2, 1h ; Check if area is Wygol or not.
+  ldr r1, =0210078Dh
+  ldrb r0, [r1] ; Load top screen from save file.
+  bne @NotInWygol
+  @InWygol:
+    cmp r0, 5h
+    moveq r0, 8h ; If the save file had the normal map, set it to Wygol map.
+    b @AfterWygol
+  @NotInWygol:
+    cmp r0, 8h
+    moveq r0, 5h ; If the save file had the Wygol map, set it to the normal map.
+  @AfterWygol:
+  mov r1, 10h
+  mov r2, 1h
+  bl 020657F8h ; Change the top screen to that value.
+  
   pop r1, r15
   .pool
 
