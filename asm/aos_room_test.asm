1bc127a7b2263a6f0ce61c7557c4f763acaaa829
LagoLunatic <LagoLunatic@users.noreply.github.com>
2017-04-23 13:26:12 -0400

    AoS: Improve room test

--- asm/aos_room_test.asm
+++ asm/aos_room_test.asm
@@ -6,27 +6,30 @@
 
 .org 0x08002B54 ; Nintendo logo game state update code.
   push r4, r5, r14
-  ; Set the game state to 02-06 (file select menu).
+  
+  mov r0, 0h ; Save file 0.
+  bl 08012744h ; Call function to load save file.
+  
   mov r0, 2h
-  ldr r1, =02000010h
-  strb r0, [r1]
-  mov r0, 6h
-  strb r0, [r1, 1h]
+  mov r1, 7h
+  bl 08001980h ; Call function to get room pointer by sector index r0 and room index r1.
+  ldr r1, =020003CCh
+  str r0, [r1] ; Store it as the current room at 020003CC.
   
-  ; Save file 1.
-  mov r0, 1h
-  lsl r0, r0, 1h ; Multiply by 2 because 2 means save file 1 and 4 means save file 2.
-  ldr r1, =020004F9h ; The selected save file.
-  strb r0, [r1]
+  ;ldr r1, =02000524h
+  ;ldr r0, =80001h
+  ;str r0, [r1]
+  ;ldr r0, =60001h
+  ;str r0, [r1, 4h]
   
-  ; Return -1 so the state manager doesn't change the state to something else besides 02-06.
-  mov r0, 1h
-  neg r0, r0
+  ; Return 4 so the state manager changes the state to ingame.
+  mov r0, 4h
   
   pop r4, r5, r15
   .pool
 
-.org 0x080075B4 ; Inside file select menu code.
-  nop ; Remove check that the player just pressed the A button; load the save instantly instead.
+; Allow skipping events with start.
+.org 0x0805B56C
+  mov r0, 3h
 
 .close
