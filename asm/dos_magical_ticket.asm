37af30a2a51bb6ba79248e7ff8f30f2a0f87988f
LagoLunatic <LagoLunatic@users.noreply.github.com>
2017-08-06 18:13:47 -0400

    Improvements to DoS magical tickets, update docs

--- asm/dos_magical_ticket.asm
+++ asm/dos_magical_ticket.asm
@@ -15,12 +15,17 @@
 .open "ftc/overlay9_41", @Overlay41Start
 
 .org @FreeSpace
-  ; TODO: Don't allow using during boss fights.
+  ; Don't let the player use magical tickets during boss fights.
+  ldr r0, =020F6DFCh
+  ldr r0, [r0] ; Load the bitfield of global flags.
+  ldr r1, =0C0040007h ; Check the flags to make sure we're not in the middle of a boss fight, an event, and that the player has saved the game at least once.
+  ands r0, r0, r1
+  bne 0x021EF0F8 ; Return to the consumable code if any of those bits are set.
   
   mov r0, 0h ; Sector index
-  mov r1, 1h ; Room index
-  mov r2, 80h ; X pos
-  mov r3, 60h ; Y pos
+  mov r1, 0Ah ; Room index
+  mov r2, 78h ; X pos
+  mov r3, 9Fh ; Y pos
   bl 02026AD0h ; SetDestinationRoomSectorAndRoomIndexes
   ldr r1, =020F6DF4h
   mov r0, 6h
@@ -31,9 +36,9 @@
   ; Enable control of the player, in case they were using the slide puzzle and controls were disabled.
   bl 021F64BCh
   
-  ; Below is copy pasted, it triggers the game to unpause.
-  mov     r0,44h
-  bl      2029BF0h
+  ; Below block is copy pasted, it triggers the game to unpause.
+  ; mov     r0,44h
+  ; bl      2029BF0h ; These two lines play the "unpausing" SFX, but we comment these out because we want the "use consumable" SFX instead.
   mov     r0,0h
   mvn     r1,0Fh
   mov     r2,8h
@@ -45,7 +50,7 @@
   ldrb    r0,[r1,8h]
   cmp     r0,17h
   strneb  r2,[r1,0Ah]
-  ; Above is copy pasted, it triggers the game to unpause
+  ; Above block is copy pasted, it triggers the game to unpause.
   
   mov r4, 42h ; This tells the rest of the consumable code to use one of the consumable, and play the consumable-used SFX.
   b 0x021EF0F8 ; Return to the consumable code.
