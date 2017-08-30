3df7ace1a35ba87ea1eda658f1dbf1f221ef8e0d
LagoLunatic <LagoLunatic@users.noreply.github.com>
2017-08-30 12:27:38 -0400

    Improve tileset editor GUI, allow selecting multiple tiles

--- dsvedit/tileset_editor_dialog.rb
+++ dsvedit/tileset_editor_dialog.rb
@@ -5,8 +5,12 @@ class TilesetEditorDialog < Qt::Dialog
   BACKGROUND_BRUSH = Qt::Brush.new(Qt::Color.new(200, 200, 200, 255))
   RED_PEN_COLOR = Qt::Pen.new(Qt::Color.new(255, 0, 0))
   
-  slots "select_tile_by_x_y(int, int, const Qt::MouseButton&)"
-  slots "select_tile_index_on_gfx_page(int, int, const Qt::MouseButton&)"
+  slots "mouse_clicked_on_tileset(int, int, const Qt::MouseButton&)"
+  slots "mouse_moved_on_tileset(int, int, const Qt::MouseButton&)"
+  slots "mouse_released_on_tileset(int, int, const Qt::MouseButton&)"
+  slots "mouse_clicked_on_gfx_page(int, int, const Qt::MouseButton&)"
+  slots "mouse_moved_on_gfx_page(int, int, const Qt::MouseButton&)"
+  slots "mouse_released_on_gfx_page(int, int, const Qt::MouseButton&)"
   slots "gfx_page_changed(int)"
   slots "palette_changed(int)"
   slots "toggle_flips(bool)"
@@ -26,16 +30,19 @@ class TilesetEditorDialog < Qt::Dialog
     
     @tileset_graphics_scene = ClickableGraphicsScene.new
     @ui.tileset_graphics_view.setScene(@tileset_graphics_scene)
+    @ui.tileset_graphics_view.setMouseTracking(true) # Detect mouse move even when not clicking
     @tileset_graphics_scene.setBackgroundBrush(BACKGROUND_BRUSH)
-    connect(@tileset_graphics_scene, SIGNAL("clicked(int, int, const Qt::MouseButton&)"), self, SLOT("select_tile_by_x_y(int, int, const Qt::MouseButton&)"))
-    connect(@tileset_graphics_scene, SIGNAL("moved(int, int, const Qt::MouseButton&)"), self, SLOT("select_tile_by_x_y(int, int, const Qt::MouseButton&)"))
+    connect(@tileset_graphics_scene, SIGNAL("clicked(int, int, const Qt::MouseButton&)"), self, SLOT("mouse_clicked_on_tileset(int, int, const Qt::MouseButton&)"))
+    connect(@tileset_graphics_scene, SIGNAL("moved(int, int, const Qt::MouseButton&)"), self, SLOT("mouse_moved_on_tileset(int, int, const Qt::MouseButton&)"))
+    connect(@tileset_graphics_scene, SIGNAL("released(int, int, const Qt::MouseButton&)"), self, SLOT("mouse_released_on_tileset(int, int, const Qt::MouseButton&)"))
     
     @gfx_page_graphics_scene = ClickableGraphicsScene.new
     @ui.gfx_page_graphics_view.scale(3, 3)
     @ui.gfx_page_graphics_view.setScene(@gfx_page_graphics_scene)
     @gfx_page_graphics_scene.setBackgroundBrush(BACKGROUND_BRUSH)
-    connect(@gfx_page_graphics_scene, SIGNAL("clicked(int, int, const Qt::MouseButton&)"), self, SLOT("select_tile_index_on_gfx_page(int, int, const Qt::MouseButton&)"))
-    connect(@gfx_page_graphics_scene, SIGNAL("moved(int, int, const Qt::MouseButton&)"), self, SLOT("select_tile_index_on_gfx_page(int, int, const Qt::MouseButton&)"))
+    connect(@gfx_page_graphics_scene, SIGNAL("clicked(int, int, const Qt::MouseButton&)"), self, SLOT("mouse_clicked_on_gfx_page(int, int, const Qt::MouseButton&)"))
+    connect(@gfx_page_graphics_scene, SIGNAL("moved(int, int, const Qt::MouseButton&)"), self, SLOT("mouse_moved_on_gfx_page(int, int, const Qt::MouseButton&)"))
+    connect(@gfx_page_graphics_scene, SIGNAL("released(int, int, const Qt::MouseButton&)"), self, SLOT("mouse_released_on_gfx_page(int, int, const Qt::MouseButton&)"))
     
     @selected_tile_graphics_scene = Qt::GraphicsScene.new
     @ui.selected_tile_graphics_view.scale(3, 3)
@@ -86,6 +93,12 @@ class TilesetEditorDialog < Qt::Dialog
     
     @collision_mode = false
     
+    @selected_tiles = []
+    @selection_x = 0
+    @selection_y = 0
+    @selection_width = 0
+    @selection_height = 0
+    
     self.show()
     
     @room = room
@@ -97,13 +110,16 @@ class TilesetEditorDialog < Qt::Dialog
     
     if SYSTEM == :nds
       @tile_width = @tile_height = 16
-      @tiles_per_row = 16
+      @tileset_width = 16
+      @tileset_height = 64
       @tiles_per_gfx_page_row = 8
     else
       @tile_width = @tile_height = 8
-      @tiles_per_row = 16*4
+      @tileset_width = 16*4
+      @tileset_height = 64
       @tiles_per_gfx_page_row = 16
     end
+    @tileset_graphics_scene.setSceneRect(0, 0, @tileset_width*@tile_width, @tileset_height*@tile_height)
     
     layer = room.layers.first
     if layer
@@ -235,9 +251,9 @@ class TilesetEditorDialog < Qt::Dialog
       
       render_tile_on_tileset(index_on_tileset)
       
-      x_on_tileset = index_on_tileset % @tiles_per_row
-      y_on_tileset = index_on_tileset / @tiles_per_row
-      tile_pixmap_item.setPos(x_on_tileset*@tile_width + @tile_width/2, y_on_tileset*@tile_height + @tile_height/2)
+      x_on_tileset = index_on_tileset % @tileset_width
+      y_on_tileset = index_on_tileset / @tileset_width
+      tile_pixmap_item.setPos(x_on_tileset*@tile_width, y_on_tileset*@tile_height)
       
       @tileset_graphics_scene.addItem(tile_pixmap_item)
     end
@@ -265,16 +281,15 @@ class TilesetEditorDialog < Qt::Dialog
       blob = chunky_coll_tile.to_blob
       pixmap.loadFromData(blob, blob.length)
       tile_pixmap_item.pixmap = pixmap
-      
-      tile_pixmap_item.setOffset(-@tile_width/2, -@tile_height/2)
     else
-      render_tile(tile, tile_pixmap_item)
+      render_tile_to_pixmap_item(tile, tile_pixmap_item)
     end
   end
   
-  def render_tile(tile, tile_pixmap_item)
+  def render_tile(tile)
     gfx = @gfx_pages[tile.tile_page]
     if gfx.nil?
+      puts "GFX is nil, tileset %08X" % @tileset_pointer
       return # TODO: figure out why this sometimes happens.
     end
     
@@ -289,8 +304,9 @@ class TilesetEditorDialog < Qt::Dialog
       palette = @palettes_256[tile.palette_index]
     end
     if palette.nil?
+      # TODO: figure out why this sometimes happens.
       puts "Palette index #{tile.palette_index} out of range, tileset %08X" % @tileset_pointer
-      return # TODO: figure out why this sometimes happens.
+      palette = @renderer.generate_palettes(nil, 16).first # Dummy red palette
     end
     
     if SYSTEM == :nds
@@ -309,21 +325,48 @@ class TilesetEditorDialog < Qt::Dialog
       chunky_tile = @renderer.render_1_dimensional_minitile(gfx_wrapper, palette, minitile_index_on_page)
     end
     
+    if tile.horizontal_flip
+      chunky_tile.flip_vertically! # Flips it horizontally despite the name
+    end
+    if tile.vertical_flip
+      chunky_tile.flip_horizontally! # Flips it vertically despite the name
+    end
+    
+    return chunky_tile
+  end
+  
+  def render_tile_to_pixmap_item(tile, tile_pixmap_item)
+    chunky_tile = render_tile(tile)
+    return if chunky_tile.nil?
+    
     pixmap = Qt::Pixmap.new
     blob = chunky_tile.to_blob
     pixmap.loadFromData(blob, blob.length)
     tile_pixmap_item.pixmap = pixmap
+  end
+  
+  def render_selected_tiles_to_cursor_item
+    chunky_tiles_rect = ChunkyPNG::Image.new(@selection_width*@tile_width, @selection_height*@tile_height)
     
-    xscale = 1
-    yscale = 1
-    if tile.horizontal_flip
-      xscale = -1
+    @selection_tiles_pixmaps = []
+    @selected_tiles.each_with_index do |tile, i|
+      chunky_tile = render_tile(tile)
+      
+      pixmap = Qt::Pixmap.new
+      blob = chunky_tile.to_blob
+      pixmap.loadFromData(blob, blob.length)
+      @selection_tiles_pixmaps << pixmap
+      
+      x = i % @selection_width
+      y = i / @selection_width
+      
+      chunky_tiles_rect.compose!(chunky_tile, x*@tile_width, y*@tile_height)
     end
-    if tile.vertical_flip
-      yscale = -1
-    end
-    tile_pixmap_item.setOffset(-@tile_width/2, -@tile_height/2)
-    tile_pixmap_item.setTransform(Qt::Transform::fromScale(xscale, yscale))
+    
+    pixmap = Qt::Pixmap.new
+    blob = chunky_tiles_rect.to_blob
+    pixmap.loadFromData(blob, blob.length)
+    @cursor_item.pixmap = pixmap
   end
   
   def load_selected_tile
@@ -336,27 +379,11 @@ class TilesetEditorDialog < Qt::Dialog
     @ui.vertical_flip.checked = @selected_tile.vertical_flip
     
     selected_tile_pixmap_item = Qt::GraphicsPixmapItem.new
-    render_tile(@selected_tile, selected_tile_pixmap_item)
+    render_tile_to_pixmap_item(@selected_tile, selected_tile_pixmap_item)
     @selected_tile_graphics_scene.addItem(selected_tile_pixmap_item)
     
     tile_x_pos_on_page = @selected_tile.index_on_tile_page % @tiles_per_gfx_page_row
     tile_y_pos_on_page = @selected_tile.index_on_tile_page / @tiles_per_gfx_page_row
-    
-    @gfx_page_graphics_scene.removeItem(@gfx_selection_rectangle) if @gfx_selection_rectangle
-    @gfx_selection_rectangle = Qt::GraphicsRectItem.new
-    @gfx_selection_rectangle.setPen(RED_PEN_COLOR)
-    @gfx_selection_rectangle.setRect(0, 0, @tile_width, @tile_height)
-    @gfx_selection_rectangle.setPos(tile_x_pos_on_page*@tile_width, tile_y_pos_on_page*@tile_height)
-    @gfx_page_graphics_scene.addItem(@gfx_selection_rectangle)
-    
-    @tileset_graphics_scene.removeItem(@selected_tile_selection_rectangle) if @selected_tile_selection_rectangle
-    @selected_tile_selection_rectangle = Qt::GraphicsRectItem.new
-    @selected_tile_selection_rectangle.setPen(RED_PEN_COLOR)
-    @selected_tile_selection_rectangle.setRect(0, 0, @tile_width, @tile_height)
-    tile_x_pos_on_tileset = @selected_tile_index % @tiles_per_row
-    tile_y_pos_on_tileset = @selected_tile_index / @tiles_per_row
-    @selected_tile_selection_rectangle.setPos(tile_x_pos_on_tileset*@tile_width, tile_y_pos_on_tileset*@tile_height)
-    @tileset_graphics_scene.addItem(@selected_tile_selection_rectangle)
     
     return if SYSTEM == :gba
     
@@ -448,22 +475,165 @@ class TilesetEditorDialog < Qt::Dialog
     gfx_page_changed(@selected_tile.tile_page || 0)
     load_selected_tile()
     render_tile_on_tileset(@selected_tile_index)
+    
+    @selected_tiles.each do |tile|
+      tile.palette_index = palette_index
+    end
+    if @cursor_item
+      render_selected_tiles_to_cursor_item()
+    end
   end
   
-  def select_tile_by_x_y(x, y, button)
-    return unless (0..@tile_width*@tiles_per_row-1).include?(x) && (0..1023).include?(y)
-    return unless button == Qt::LeftButton
+  def change_tiles_by_x_y(mouse_x, mouse_y)
+    return if @selected_tiles.nil?
     
-    i = x/@tile_width + y/@tile_width*@tiles_per_row
+    @selection_height.times do |y_off|
+      @selection_width.times do |x_off|
+        x = mouse_x/@tile_width + x_off
+        y = mouse_y/@tile_height + y_off
+        
+        next if x < 0
+        break if x >= @tileset_width
+        next if y < 0
+        break if y >= @tileset_height
+        
+        # Blank tile at the start of the tileset
+        if SYSTEM == :nds && x == 0 && y == 0
+          next
+        elsif SYSTEM == :gba && x < 4 && y < 4
+          next
+        end
+        
+        i_in_selection = x_off + y_off*@selection_width
+        new_tile = @selected_tiles[i_in_selection]
+        
+        i_on_tileset = x + y*@tileset_width
+        tile = @tiles[i_on_tileset]
+        tile.index_on_tile_page = new_tile.index_on_tile_page
+        tile.tile_page = new_tile.tile_page
+        tile.horizontal_flip = new_tile.horizontal_flip
+        tile.vertical_flip = new_tile.vertical_flip
+        tile.palette_index = new_tile.palette_index
+        
+        pixmap = @selection_tiles_pixmaps[i_in_selection]
+        tile_pixmap_item = @tileset_pixmap_items[i_on_tileset]
+        tile_pixmap_item.pixmap = pixmap
+      end
+    end
+  end
+  
+  def mouse_clicked_on_tileset(x, y, button)
+    return unless (0..@tileset_width*@tile_width-1).include?(x) && (0..@tileset_height*@tile_height-1).include?(y)
+    
+    case button
+    when Qt::LeftButton
+      change_tiles_by_x_y(x, y)
+    when Qt::RightButton
+      right_mouse_clicked_on_tileset(x, y)
+    end
+  end
+  
+  def mouse_moved_on_tileset(x, y, button)
+    update_tileset_cursor(x, y, button)
+    
+    if button & Qt::RightButton != 0
+      right_mouse_moved_on_tileset(x, y)
+    elsif button & Qt::LeftButton != 0
+      change_tiles_by_x_y(x, y)
+    end
+  end
+  
+  def right_mouse_clicked_on_tileset(mouse_x, mouse_y)
+    i = mouse_x/@tile_width + mouse_y/@tile_width*@tileset_width
     
     select_tile(i)
+    
+    if !@cursor_item
+      @cursor_item = Qt::GraphicsPixmapItem.new
+      @tileset_graphics_scene.addItem(@cursor_item)
+    end
+    
+    @selection_origin = Qt::Point.new(mouse_x/@tile_width, mouse_y/@tile_height)
+    
+    @selection_rectangle = Qt::GraphicsRectItem.new
+    @selection_rectangle.setPen(RED_PEN_COLOR)
+    @selection_rectangle.setRect(0, 0, @tile_width, @tile_height)
+    @tileset_graphics_scene.addItem(@selection_rectangle)
+    
+    update_selection_on_tileset(mouse_x, mouse_y)
+  end
+  
+  def right_mouse_moved_on_tileset(x, y)
+    update_selection_on_tileset(x, y)
+  end
+  
+  def mouse_released_on_tileset(x, y, button)
+    case button
+    when Qt::RightButton
+      stop_selecting()
+    end
+  end
+  
+  def keyPressEvent(event)
+    case event.key
+    when Qt::Key_X
+      # Flip selection horizontally.
+      if @selected_tiles
+        new_selected_tiles = []
+        
+        @selection_height.times do |y|
+          @selection_width.times.reverse_each do |x|
+            i = x + y*@selection_width
+            tile = @selected_tiles[i]
+            tile.horizontal_flip = !tile.horizontal_flip
+            new_selected_tiles << tile
+          end
+        end
+        
+        @selected_tiles = new_selected_tiles
+        render_selected_tiles_to_cursor_item()
+      end
+    when Qt::Key_Y
+      # Flip selection vertically.
+      if @selected_tiles
+        new_selected_tiles = []
+        
+        @selection_height.times.reverse_each do |y|
+          @selection_width.times do |x|
+            i = x + y*@selection_width
+            tile = @selected_tiles[i]
+            tile.vertical_flip = !tile.vertical_flip
+            new_selected_tiles << tile
+          end
+        end
+        
+        @selected_tiles = new_selected_tiles
+        render_selected_tiles_to_cursor_item()
+      end
+    end
+    
+    super(event)
+  end
+  
+  def update_tileset_cursor(x, y, button)
+    return if @cursor_item.nil?
+    
+    if (0..@tileset_width*@tile_width-1).include?(x) && (0..@tileset_height*@tile_height-1).include?(y)
+      @cursor_item.show()
+    else
+      @cursor_item.hide()
+    end
+    
+    x = x/@tile_width*@tile_width
+    y = y/@tile_height*@tile_height
+    @cursor_item.setPos(x, y)
   end
   
   def select_tile(tile_index)
     old_selected_tile = @selected_tile
     
     @selected_tile_index = tile_index
-    @selected_tile = @tiles[tile_index]
+    @selected_tile = @tiles[tile_index].dup
     @selected_collision_tile = @collision_tiles[tile_index]
     
     @ui.gfx_page_index.setCurrentIndex(@selected_tile.tile_page)
@@ -477,14 +647,99 @@ class TilesetEditorDialog < Qt::Dialog
     load_selected_tile()
   end
   
-  def select_tile_index_on_gfx_page(x, y, button)
-    return unless (0..127).include?(x) && (0..127).include?(y)
+  def update_selection_rectangle(mouse_x, mouse_y, max_w, max_h)
+    mouse_x = [mouse_x, 0].max
+    mouse_y = [mouse_y, 0].max
+    mouse_x = [mouse_x, max_w].min
+    mouse_y = [mouse_y, max_h].min
+    mouse_x = mouse_x/@tile_width
+    mouse_y = mouse_y/@tile_height
     
-    i = x/@tile_width + y/@tile_width*@tiles_per_gfx_page_row
+    @selection_x = [mouse_x, @selection_origin.x].min
+    @selection_y = [mouse_y, @selection_origin.y].min
+    @selection_width = [mouse_x, @selection_origin.x].max - @selection_x + 1
+    @selection_height = [mouse_y, @selection_origin.y].max - @selection_y + 1
+    
+    @selection_rectangle.setRect(@selection_x*@tile_width, @selection_y*@tile_height, @selection_width*@tile_width, @selection_height*@tile_height)
+  end
+  
+  def update_selection_on_tileset(mouse_x, mouse_y)
+    return unless @selection_origin
+    
+    max_w = @tileset_width*@tile_width
+    max_h = @tileset_height*@tile_height
+    update_selection_rectangle(mouse_x, mouse_y, max_w, max_h)
+    
+    @selected_tiles = []
+    @selection_height.times do |y_off|
+      @selection_width.times do |x_off|
+        curr_x = @selection_x + x_off
+        curr_y = @selection_y + y_off
+        i = curr_x + curr_y*@tileset_width
+        tile = @tiles[i].dup
+        @selected_tiles << tile
+      end
+    end
+  end
+  
+  def update_selection_on_gfx_page(mouse_x, mouse_y)
+    return unless @selection_origin
+    
+    max_w = max_h = 127
+    update_selection_rectangle(mouse_x, mouse_y, max_w, max_h)
+    
+    @selected_tiles = []
+    @selection_height.times do |y_off|
+      @selection_width.times do |x_off|
+        curr_x = @selection_x + x_off
+        curr_y = @selection_y + y_off
+        i = curr_x + curr_y*@tiles_per_gfx_page_row
+        tile = @selected_tile.dup
+        tile.index_on_tile_page = i
+        @selected_tiles << tile
+      end
+    end
+  end
+  
+  def stop_selecting
+    @selection_origin = nil
+    
+    render_selected_tiles_to_cursor_item()
+    
+    @gfx_page_graphics_scene.removeItem(@selection_rectangle) if @selection_rectangle
+    @tileset_graphics_scene.removeItem(@selection_rectangle) if @selection_rectangle
+  end
+  
+  def mouse_clicked_on_gfx_page(mouse_x, mouse_y, button)
+    return unless (0..127).include?(mouse_x) && (0..127).include?(mouse_y)
+    
+    i = mouse_x/@tile_width + mouse_y/@tile_width*@tiles_per_gfx_page_row
     @selected_tile.index_on_tile_page = i
     
     load_selected_tile()
-    render_tile_on_tileset(@selected_tile_index)
+    
+    
+    if !@cursor_item
+      @cursor_item = Qt::GraphicsPixmapItem.new
+      @tileset_graphics_scene.addItem(@cursor_item)
+    end
+    
+    @selection_origin = Qt::Point.new(mouse_x/@tile_width, mouse_y/@tile_height)
+    
+    @selection_rectangle = Qt::GraphicsRectItem.new
+    @selection_rectangle.setPen(RED_PEN_COLOR)
+    @selection_rectangle.setRect(0, 0, @tile_width, @tile_height)
+    @gfx_page_graphics_scene.addItem(@selection_rectangle)
+    
+    update_selection_on_gfx_page(mouse_x, mouse_y)
+  end
+  
+  def mouse_moved_on_gfx_page(mouse_x, mouse_y, button)
+    update_selection_on_gfx_page(mouse_x, mouse_y)
+  end
+  
+  def mouse_released_on_gfx_page(mouse_x, mouse_y, button)
+    stop_selecting()
   end
   
   def toggle_flips(checked)
