1cdeff57f734d47fd4dbe6947f16f9d43f6f6e72
LagoLunatic <LagoLunatic@users.noreply.github.com>
2018-01-21 17:45:39 -0500

    Add patches to fix map explore bug in DoS and PoR

---
+++ asm/dos_fix_map_explore_bug.asm
@@ -0,0 +1,48 @@
+.nds
+.relativeinclude on
+.erroronwarning on
+
+; This patch fixes a bug in DoS where the map will consider the player to be in the next room over if they are on the very edge of the screen.
+; This bug causes the glitch in the room randomizer (or sliding puzzle in the vanilla game) where walking through a door will reveal the map tile of the room you would have normally entered without room randomizer/sliding puzzle.
+
+@Overlay41Start equ 0x02308920
+@FreeSpace equ @Overlay41Start + 0x80
+
+.open "ftc/arm9.bin", 02000000h
+
+.org 0x020226B4
+  b @ClampXPosToRoomWidth
+.org 0x020226C4
+  b @ClampYPosToRoomHeight
+
+.close
+
+.open "ftc/overlay9_41", @Overlay41Start
+
+.org @FreeSpace
+@ClampXPosToRoomWidth:
+  mov r3, r1, asr 14h ; Divide subpixels X pos by 0x100000 to get X pos in screens
+  cmp r3, 0h
+  movlt r3, 0h ; X = 0 if X < 0
+  ldr r1, =020F75A8h
+  ldr r1, [r1]
+  ldrb r1, [r1] ; Load the room's width from the collision layer (the first layer)
+  sub r1, r1, 1h
+  cmp r3, r1
+  movgt r3, r1 ; X = room_width-1 if X > room_width-1
+  add r3, r3, r5 ; Add the room's X pos to the player's X pos within the room in screens (now clamped).
+  b 20226B8h ; Return
+@ClampYPosToRoomHeight:
+  cmp r7, 0h
+  movlt r7, 0h ; Y = 0 if Y < 0
+  ldr r1, =020F75A8h
+  ldr r1, [r1]
+  ldrb r1, [r1, 1h] ; Load the room's height from the collision layer (the first layer)
+  sub r1, r1, 1h
+  cmp r7, r1
+  movgt r7, r1 ; Y = room_height-1 if Y > room_height-1
+  add r1, r7, r2 ; Add the room's Y pos to the player's Y pos within the room in screens (now clamped).
+  b 20226C8h ; Return
+  .pool
+
+.close
---
+++ asm/por_fix_map_explore_bug.asm
@@ -0,0 +1,54 @@
+.nds
+.relativeinclude on
+.erroronwarning on
+
+; This patch fixes a bug in PoR where the map will consider the player to be in the next room over if they are on the very edge of the screen.
+; This bug causes the suspend glitch, and the glitch in room randomizer where walking through a door will reveal the map tile of the room you would have normally entered in the vanilla game without room randomizer.
+
+@Overlay119Start equ 0x02308EC0
+@FreeSpace equ @Overlay119Start + 0x24
+
+.open "ftc/arm9.bin", 02000000h
+
+.org 0x0202E630
+  mov r0, r2, asr 14h ; Divide subpixels X pos by 0x100000 to get X pos in screens
+  b @ClampXPosToRoomWidth
+  add r0, r3, r0 ; Add X pos within the room in screens (now clamped) to the room's X pos on the map
+
+.org 0x0202E660
+  mov r0, r2, asr 11h ; r2 has Y pos in subpixels/6, now divide by 0x20000 to get Y pos in screens
+  b @ClampYPosToRoomHeight
+  mov r2, r0 ; Move Y pos within the room in screens (now clamped) back to r2 where it will be added to the room's Y pos on the map
+
+.close
+
+.open "ftc/overlay9_119", @Overlay119Start
+
+.org @FreeSpace
+@ClampXPosToRoomWidth:
+  push r1, r3
+  cmp r0, 0h
+  movlt r0, 0h ; X = 0 if X < 0
+  ldr r2, =02112458h
+  ldr r2, [r2]
+  ldrb r2, [r2] ; Load the room's width from the collision layer (the first layer)
+  sub r2, r2, 1h
+  cmp r0, r2
+  movgt r0, r2 ; X = room_width-1 if X > room_width-1
+  pop r1, r3
+  b 202E638h ; Return
+@ClampYPosToRoomHeight:
+  push r1, r3
+  cmp r0, 0h
+  movlt r0, 0h ; Y = 0 if Y < 0
+  ldr r2, =02112458h
+  ldr r2, [r2]
+  ldrb r2, [r2, 1h] ; Load the room's height from the collision layer (the first layer)
+  sub r2, r2, 1h
+  cmp r0, r2
+  movgt r0, r2 ; Y = room_height-1 if Y > room_height-1
+  pop r1, r3
+  b 202E668h ; Return
+  .pool
+
+.close
