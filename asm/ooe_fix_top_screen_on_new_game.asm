777ab744dae96949c4ec30125a2dfd7bac7f3774
LagoLunatic <LagoLunatic@users.noreply.github.com>
2018-01-21 21:04:06 -0500

    OoE top screen patch: Reduce transition time so map is immediately functional

--- asm/ooe_fix_top_screen_on_new_game.asm
+++ asm/ooe_fix_top_screen_on_new_game.asm
@@ -9,4 +9,7 @@
 .org 0x02214F68 ; Code run when starting a new game that specifies what top screen to set.
   mov r0, 5h ; Change to top screen 5, the normal map.
 
+.org 0x02214F50
+  mov r3, 4h ; Change the time for the transition to take from 0x78 frames to 4 frames, since the map is unusable during the long transition.
+
 .close
