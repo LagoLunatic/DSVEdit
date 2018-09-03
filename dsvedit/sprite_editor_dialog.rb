09a978d51dc7039ceefd0b8a6904b274c28506ce
LagoLunatic <LagoLunatic@users.noreply.github.com>
2018-09-03 13:10:08 -0400

    Fix error if an OoE melee glyph sprite is not assigned to any glyph

--- dsvedit/sprite_editor_dialog.rb
+++ dsvedit/sprite_editor_dialog.rb
@@ -362,7 +362,7 @@ class SpriteEditor < Qt::Dialog
     @one_dimensional_render_mode = false
     if GAME == "ooe"
       skill_item = @skill_items_for_each_gfx_index[skill_gfx_index].first
-      if skill_item["Code"] == MELEE_GLYPH_CODE && (skill_item["Unknown 4"] & 0x40) == 0
+      if skill_item && skill_item["Code"] == MELEE_GLYPH_CODE && (skill_item["Unknown 4"] & 0x40) == 0
         @transparent_trails = true
       else
         @transparent_trails = false
