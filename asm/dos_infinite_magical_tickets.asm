3a166419f274aff432b47ccc4749c99abfe451f9
LagoLunatic <LagoLunatic@users.noreply.github.com>
2020-05-31 23:03:03 -0400

    Fix DoS infinite magic ticket patch corrupting some code, causing crashes on real hardware

--- asm/dos_infinite_magical_tickets.asm
+++ asm/dos_infinite_magical_tickets.asm
@@ -3,7 +3,7 @@
 .erroronwarning on
 
 @Overlay41Start equ 0x02308920
-@FreeSpace equ @Overlay41Start + 0x7C
+@FreeSpace equ @Overlay41Start ; Note: This is intentionally put at the same location as "dos_magical_ticket.asm".
 
 .open "ftc/overlay9_0", 0219E3E0h
 
@@ -25,13 +25,13 @@
 
 .open "ftc/overlay9_41", @Overlay41Start
 
-; Don't consume magical tickets on use.
-.org @FreeSpace
-  mov r0, 42h ; This is the SFX that will be played on use.
-  b 0x021EF264 ; Return to the consumable code after the part where it would remove the item from your inventory.
-
 ; Change the global flags checked to not care that the game hasn't been saved once yet (bit 0x40000000). In its place we check the bit that makes soul drops have a 100% drop chance (bit 0x10000000), so that the player can't use magical tickets during the prologue.
 .org @FreeSpace+0x08
   mov r1, 90000007h
+
+; Don't consume magical tickets on use.
+.org @FreeSpace+0x7C
+  mov r0, 42h ; This is the SFX that will be played on use (needs to be put in a different register compared to "dos_magical_ticket.asm" because the branch destination is different).
+  b 0x021EF264 ; Return to the consumable code after the part where it would remove the item from your inventory.
 
 .close
