3e19041bd4f8ab1f26ba3c4dd32d6a9e28bea27b
LagoLunatic <LagoLunatic@users.noreply.github.com>
2023-01-01 01:54:36 -0500

    Implement ctrl+mousewheel zooming in/out for rooms
    fixes #68

--- dsvedit/room_view.rb
+++ dsvedit/room_view.rb
@@ -1,7 +1,21 @@
 
 class RoomView < Qt::GraphicsView
+  ZOOM_SCALES = [
+    0.25,
+    0.5,
+    0.75,
+    1.0,
+    1.5,
+    2.0,
+    2.5,
+    3.0,
+    4.0,
+  ]
+  
   def initialize(parent)
     super(parent)
+    
+    @curr_zoom_index = ZOOM_SCALES.index(1.0)
     
     # We want to receive mouseMoveEvents even when the user is not clicking any mouse buttons.
     setMouseTracking(true)
@@ -69,5 +83,39 @@ class RoomView < Qt::GraphicsView
     end
     
     super(event)
+  end
+  
+  def wheelEvent(event)
+    if Qt::Application.keyboardModifiers() & Qt::ControlModifier == 0
+      super(event)
+      return
+    end
+    
+    orig_view_pos = event.pos()
+    orig_scene_pos = mapToScene(orig_view_pos)
+    
+    y_change = event.delta()
+    
+    old_zoom_scale = ZOOM_SCALES[@curr_zoom_index]
+    if y_change > 0
+      if @curr_zoom_index == ZOOM_SCALES.size - 1
+        return
+      end
+      @curr_zoom_index += 1
+    elsif y_change < 0
+      if @curr_zoom_index == 0
+        return
+      end
+      @curr_zoom_index -= 1
+    else
+      return
+    end
+    
+    curr_zoom_scale = ZOOM_SCALES[@curr_zoom_index]
+    scale_mult = curr_zoom_scale / old_zoom_scale
+    
+    scale(scale_mult, scale_mult)
+    
+    updateSceneRect(scene().sceneRect())
   end
 end
